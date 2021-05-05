5782094dff008bbc447447f3ea51770279e8b3b7
Kyonyx12 <themisteryofworld@gmail.com>
2021-05-04 22:50:39 -0700

    seven commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import About from "./components/about/About";
 import Main from "./components/main/Main";
 import Nav from "./components/main/nav/Nav";
